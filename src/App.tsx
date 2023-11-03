d5d6707b210db42110ada12cd1205112cb8ca02d
Frank <fgkiron@gmail.com>
2023-11-03 18:13:19 +0300

    :sparkles: Update App component with added Footer component and comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Footer } from './viewModel/components/footer'; 
 
 function App() {
   return (
@@ -19,7 +20,10 @@ function App() {
           Learn React
         </a>
       </header>
+      
+      <Footer/>
     </div>
+    
   );
 }
 
