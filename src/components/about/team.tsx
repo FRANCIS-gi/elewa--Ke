c4421ac6b18d8303f2805e3de81d421a3778ea56
Frank <fgkiron@gmail.com>
2023-11-06 01:20:29 +0300

    :sparkles: Add team component

---
+++ src/components/about/team.tsx
@@ -0,0 +1,149 @@
+// Import React and image assets
+import React from 'react'
+import { IMG_9755_vfprhm, jenteElewaWbqyyi, } from '../../assets/images/about';
+import { chesaElewaSoeb8f, joyGvbweu, peterElewaZuzovx } from '../../assets/images/about';
+import { frankElewaKihn9e, kenElewaYdwzop, reaganElewaBn8xkl } from '../../assets/images/about';
+import { lemmyOosngt, shawnElewaTt3iit } from '../../assets/images/about';
+import { ianElewaBejey9,  zipElewaJksbfg } from '../../assets/images/about';
+import { janeElewaYyybka, noellaElewaG6tquq, jeffElewaHdupp7 } from '../../assets/images/about';
+import  Linkedin  from "../../assets/images/social/LinkedIn.png";
+
+export const Team = (): JSX.Element =>{
+  return (
+    <div>
+        <div>
+            <div>
+                <div>
+                    {/* Display an image and information for Jente Rosseel */}
+                    <img className="about-icons" alt="Globe" src={jenteElewaWbqyyi} />
+                    <p className="about-h">Jente Rosseel </p>
+                    <p className="about-p">Founder, Managing Partner</p>
+                    <a href="https://www.linkedin.com/in/jrosseel/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin} />
+                    </a>                        
+                </div>
+                <div>
+                    {/* Display an image and information for Kennedy Adhola */}
+                    <img className="about-icons" alt="Globe" src={kenElewaYdwzop} />
+                    <p className="about-h">Kennedy Adhola </p>
+                    <p className="about-p">Operations & Partnerships</p>
+                    <a href="https://www.linkedin.com/in/kennedy-adhola-97570535/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin} />
+                    </a>                        
+                </div>
+                <div>
+                    {/* Display an image and information for Peter Reinartz */}
+                    <img className="about-icons" alt="Globe" src={peterElewaZuzovx} />
+                    <p className="about-h">Peter Reinartz </p>
+                    <p className="about-p">Executive Chairman</p>
+                    <a href="https://www.linkedin.com/in/peter-reinartz-1b4452ab/" target="_blank" rel="noopener noreferrer">
+                    <img className="img" alt="LinkedIn" src={Linkedin} />
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div>
+                    {/* Display an image and information for Noella Mwanzia */}
+                    <img className="about-icons" alt="Globe" src={noellaElewaG6tquq} />
+                    <p className="about-h">Noella Mwanzia</p>
+                    <p className="about-p">Software Engineer</p>
+                    <a href="https://www.linkedin.com/in/noella-mwanzia-633803173/" target="_blank" rel="noopener noreferrer">
+                    <img className="img" alt="LinkedIn" src={Linkedin} />
+                    </a>
+                </div>
+                
+                <div>
+                    <img className="about-icons" alt="Globe" src={ianElewaBejey9} />
+                    <p className="about-h">Ian Odhiambo</p>
+                    <p className="about-p">Founder Kujali</p>
+                    <a href="https://www.linkedin.com/in/ian-odhiambo-obutho/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin} />
+                    </a>                        
+                </div>
+                <div>
+                    <img className="about-icons" alt="Globe" src={janeElewaYyybka} />
+                    <p className="about-h">Jane Njoroge</p>
+                    <p className="about-p">Software Developer</p>
+                    <a href="https://www.linkedin.com/in/jane-w-njoroge/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin} />
+                    </a>                        
+                </div>
+                <div>
+                    <img className="about-icons" alt="Globe" src={joyGvbweu} />
+                    <p className="about-h">Joy Wawira</p>
+                    <p className="about-p">Software Developer</p>
+                    <a href="https://www.linkedin.com/in/joy-wawira/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin} />
+                    </a>                        
+                </div>
+                <div>
+                    <img className="about-icons" alt="Globe" src={IMG_9755_vfprhm} />
+                    <p className="about-h">Lynn Anastasia</p>
+                    <p className="about-p">Software Developer</p>
+                    <a href="https://www.linkedin.com/in/lynn-nyangon/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin} />
+                    </a>                        
+                </div>
+                <div>
+                    <img className="about-icons" alt="Globe" src={shawnElewaTt3iit} />
+                    <p className="about-h">Shawn Chiama </p>
+                    <p className="about-p">Software Engineer</p>
+                    <a href="https://www.linkedin.com/company/elewa/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin} />
+                    </a>                        
+                </div>
+
+                <div>
+                    <img className="about-icons" alt="Globe" src={reaganElewaBn8xkl} />
+                    <p className="about-h">Reagan Charana</p>
+                    <p className="about-p">Software Engineer </p>
+                    <a href=" https://www.linkedin.com/in/reagan-charana/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin}/>
+                    </a>                        
+                </div>
+                <div>
+                    <img className="about-icons" alt="Globe" src={chesaElewaSoeb8f} />
+                    <p className="about-h">Paul Chesa</p>
+                    <p className="about-p">Software Engineer </p>
+                    <a href="https://www.linkedin.com/in/paulchesa/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin}/>
+                    </a>                        
+                </div>
+                <div>
+                    <img className="about-icons" alt="Globe" src={jeffElewaHdupp7} />
+                    <p className="about-h">Jeff King'ori</p>
+                    <p className="about-p">Software Engineer </p>
+                    <a href="https://www.linkedin.com/in/jeffrey-king-ori-165333164/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin}/>
+                    </a>                        
+                </div>
+                <div>
+                    <img className="about-icons" alt="Globe" src={zipElewaJksbfg} />
+                    <p className="about-h">Zippie Ntabo</p>
+                    <p className="about-p">Office Administrator & Finance</p>
+                    <a href="https://www.linkedin.com/in/zippie-ntabo-310693134/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin}/>
+                    </a>                        
+                </div>
+                <div>
+                    <img className="about-icons" alt="Globe" src={frankElewaKihn9e} />
+                    <p className="about-h">Frankline Odero</p>
+                    <p className="about-p">Software Developer </p>
+                    <a href="https://www.linkedin.com/in/frankline-sable/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin}/>
+                    </a>                        
+                </div>
+                <div>
+                    <img className="about-icons" alt="Globe" src={lemmyOosngt} />
+                    <p className="about-h">Stephen Mwaura</p>
+                    <p className="about-p">Software Engineer </p>
+                    <a href="https://www.linkedin.com/in/stephenlemmymwaura/" target="_blank" rel="noopener noreferrer">
+                        <img className="img" alt="LinkedIn" src={Linkedin}/>
+                    </a>                        
+                </div>
+                
+            </div>    
+        </div>
+    </div>                
+  )
+}
