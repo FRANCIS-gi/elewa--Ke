b17a64568eb03d4a6022be6339704836a8e0a2f9
Frank <fgkiron@gmail.com>
2023-11-06 01:54:11 +0300

    :sparkles: Add shared.ts

---
+++ src/models/about/shared.ts
@@ -0,0 +1,8 @@
+export { Header } from '../../components/shared/header';
+export { HomePage } from '../../components/shared/homePage';
+export { Footer } from '../../components/shared/footer';
+export { Container } from '../../components/shared/container';
+export { Button } from '../../components/shared/button';
+export { ButtonCircle } from '../../components/shared/buttoncircle';
+export {Box} from '../../components/shared/box';
+export {ElewaLogo} from '../../components/shared/elewaLogo'
